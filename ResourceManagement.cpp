a820ae2c09d5a9921acef3fc6a9f5284ea0def15
jpmistrett <43559619+jpmistrett@users.noreply.github.com>
2021-06-24 04:51:05 -0500

    Core files

---
+++ ResourceManagement.cpp
@@ -0,0 +1,174 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ResourceManagement.h"
+#include "Math/UnrealMathUtility.h"
+#include <cstdlib>
+#include <ctime>
+
+UResourceManagement::UResourceManagement(const FObjectInitializer& ObjectInitializer): UUserWidget(ObjectInitializer)
+{
+	
+}
+
+bool UResourceManagement::Initialize()
+{
+	Super::Initialize();
+
+	QuantumController = Cast<AQuantumPlayerController>(GetWorld()->GetFirstPlayerController());
+
+	if (QuantumController != nullptr)
+	{
+		QuantumController->SetRMSReference(this);
+	}
+
+	if (CompleteManagement != nullptr) 
+	{
+		CompleteManagement->OnClicked.AddDynamic(this, &UResourceManagement::CompleteResourceManagement);
+	}
+
+	SetStartValues();
+
+	return true;
+}
+
+//call from UMG on add to viewport
+void UResourceManagement::SetStartValues()
+{
+	BaseShieldValue = QuantumController->GetShieldHealth();
+	CurrentShieldValue = BaseShieldValue;
+
+	RepairAllocation = 0.0f;
+	ScavengeAllocation = 0.0f;
+	RescueAllocation = 0.0f;
+
+	CurrentResourcePool = MaxResourcePoolValue;
+	ResourcesVisual->SetPercent(1);
+}
+
+void UResourceManagement::UpdateResourcePool(int value)
+{
+	CurrentResourcePool += value;
+	
+	CurrentResourcePool = FMath::Clamp(CurrentResourcePool, 0.0f, MaxResourcePoolValue);
+
+	UpdateResourcesVisual();
+}
+
+void UResourceManagement::UpdateRepairAllocation(int value)
+{
+	if (value > 0)
+	{
+		if (CurrentResourcePool > 0 && CurrentShieldValue < MaxShieldValue)
+		{
+			RepairAllocation += value;
+
+			RepairAllocation = FMath::Clamp(RepairAllocation, 0.0f, MaxResourcePoolValue);
+
+			UpdateResourcePool(-value);
+		}
+	}
+	else
+	{
+		if (RepairAllocation > 0)
+		{
+			RepairAllocation += value;
+
+			RepairAllocation = FMath::Clamp(RepairAllocation, 0.0f, MaxResourcePoolValue);
+
+			UpdateResourcePool(-value);
+		}
+	}
+
+	SetShieldRepairValue();
+}
+
+void UResourceManagement::UpdateScavengeAllocation(int value)
+{
+	if (value > 0)
+	{
+		if (CurrentResourcePool > 0)
+		{
+			ScavengeAllocation += value;
+
+			ScavengeAllocation = FMath::Clamp(ScavengeAllocation, 0.0f, MaxResourcePoolValue);
+
+			UpdateResourcePool(-value);
+		}
+	}
+	else
+	{
+		if (ScavengeAllocation > 0)
+		{
+			ScavengeAllocation += value;
+
+			ScavengeAllocation = FMath::Clamp(ScavengeAllocation, 0.0f, MaxResourcePoolValue);
+
+			UpdateResourcePool(-value);
+		}
+	}
+}
+
+void UResourceManagement::UpdateRescueAllocation(int value)
+{
+	if (value > 0)
+	{
+		if (CurrentResourcePool > 0)
+		{
+			RescueAllocation += value;
+
+			RescueAllocation = FMath::Clamp(RescueAllocation, 0.0f, MaxResourcePoolValue);
+
+			UpdateResourcePool(-value);
+		}
+	}
+	else
+	{
+		if (RescueAllocation > 0)
+		{
+			RescueAllocation += value;
+
+			RescueAllocation = FMath::Clamp(RescueAllocation, 0.0f, MaxResourcePoolValue);
+
+			UpdateResourcePool(-value);
+		}
+	}
+}
+
+void UResourceManagement::SetShieldRepairValue()
+{
+	CurrentShieldValue = (BaseShieldValue + (RepairAllocation * ShieldRepairsPerAllocation));
+
+	CurrentShieldValue = FMath::Clamp(CurrentShieldValue, 0.0f, MaxShieldValue);
+}
+
+void UResourceManagement::UpdateResourcesVisual()
+{
+	ResourcesVisual->SetPercent(CurrentResourcePool / MaxResourcePoolValue);
+}
+
+void UResourceManagement::CompleteResourceManagement()
+{
+	srand(time(0));
+
+	float scavengeRoll = rand() % 100;
+	float rescueRoll = rand() % 100;
+
+	if (scavengeRoll <= ScavengeSuccessRate)
+	{
+		float damageIncrease = ScavengeAllocation * WeaponDamageIncreasePerAllocation;
+		QuantumController->IncreasePlayerDamage(damageIncrease);
+	}
+
+	if (rescueRoll <= RescueSuccessRate)
+	{
+		float percentSuccessIncrease = RescueAllocation * PercentIncreasePerAllocation;
+		QuantumController->IncreasePlayerDamage(percentSuccessIncrease);
+	}
+
+	SetStartValues();
+
+	QuantumController->ShowAllocationResults();
+
+	RemoveFromParent();
+
+}
---
+++ ResourceManagement.h
@@ -0,0 +1,112 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "QuantumPlayerController.h"
+#include "Components/Button.h"
+#include "Components/ProgressBar.h"
+#include "ResourceManagement.generated.h"
+
+UCLASS()
+class MOON_API UResourceManagement : public UUserWidget
+{
+	GENERATED_BODY()
+
+	UResourceManagement(const FObjectInitializer& ObjectInitializer);
+
+	virtual bool Initialize();
+
+public:
+
+	UFUNCTION(BlueprintCallable)
+	void SetStartValues();
+
+	UFUNCTION(BlueprintCallable)
+	void UpdateResourcePool(int value);
+
+	UFUNCTION(BlueprintCallable)
+	void UpdateRepairAllocation(int value);
+
+	UFUNCTION(BlueprintCallable)
+	void UpdateScavengeAllocation(int value);
+
+	UFUNCTION(BlueprintCallable)
+	void UpdateRescueAllocation(int value);
+
+	UFUNCTION(BlueprintCallable)
+	void CompleteResourceManagement();
+
+	float BaseShieldValue;
+
+private:
+
+	void SetShieldRepairValue();
+
+	void UpdateResourcesVisual();
+
+protected:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = ResourceManagementGlobals)
+	float MaxShieldValue = 100;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = ResourceManagementGlobals)
+	float MaxResourcePoolValue = 10.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = ResourceManagementGlobals)
+	float ShieldRepairsPerAllocation = 10;
+
+	UPROPERTY(BlueprintReadWrite)
+	float CurrentResourcePool = 10.0f;
+
+	UPROPERTY(BlueprintReadOnly)
+	float CurrentShieldValue;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = ScavengeValues)
+	float ScavengeSuccessRate = 45.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = ScavengeValues)
+	float WeaponDamageIncreasePerAllocation = 0.4f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = RescueValues)
+	float RescueSuccessRate = 65.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = RescueValues)
+	float PercentIncreasePerAllocation = 2.0f;
+
+	UPROPERTY(BlueprintReadWrite, Category = ResourceManagement)
+	float RepairAllocation = 0;
+
+	UPROPERTY(BlueprintReadWrite, Category = ResourceManagement)
+	float ScavengeAllocation = 0;
+
+	UPROPERTY(BlueprintReadWrite, Category = ResourceManagement)
+	float RescueAllocation = 0;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* RepairMinus;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* RepairAdd;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* ScavengeMinus;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* ScavengeAdd;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* RescueMinus;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* RescueAdd;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* CompleteManagement;
+
+	UPROPERTY(meta = (BindWidget))
+	UProgressBar* ResourcesVisual;
+
+	AQuantumPlayerController* QuantumController;
+};
